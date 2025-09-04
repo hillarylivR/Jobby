60add98ec2a1238c74d821d504063aa690b7622d
hillarylivR <hlrodriguez@poligran.edu.co>
2025-09-03 21:15:30 -0500

    Primer Commit

---
+++ Pantallas/PantallaScript/CameraFollow.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class CameraFollow : Camera2D
+{
+	private Node2D personaje;
+
+	public override void _Ready()
+	{
+		personaje = GetNode<Node2D>("Personaje");
+	}
+	public override void _Process(double delta)
+	{
+		if (personaje != null)
+		{
+			Position = personaje.Position;
+		}
+	}
+}
